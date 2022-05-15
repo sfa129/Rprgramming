ee098373c09ca7a75f41953a1bc5916b8fab7a5a
Syed Faiz Ahmed <sfahmed129@gmail.com>
2022-05-15 23:08:09 +0500

    Few lines have been added.

--- Testing.r
+++ Testing.r
@@ -1,2 +1,8 @@
 # This is from Github
-Print("Hello from Pakistan to Github")
+print("Hello from Pakistan to Github")
+
+name = "Faiz"
+version = "Rstudio - in writing"
+
+print(name)
+print("These lines has been written in local repo")
