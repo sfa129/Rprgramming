266e5743d248b6a5311a0c06bd60dc67dcfb7500
Syed Faiz Ahmed <sfahmed129@gmail.com>
2022-05-16 18:51:11 +0500

    Salam has been added

--- Testing.r
+++ Testing.r
@@ -6,3 +6,10 @@ version = "Rstudio - in writing"
 
 print(name)
 print("These lines has been written in local repo")
+
+name = "Ahmed"
+version = "Rstudio in writing"
+print("name")
+
+#This is again from Git and Github
+print("Assalam O Alaikum, bro from Git and Github")
