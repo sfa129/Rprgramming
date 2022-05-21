bfc0d4fa68221118855e46d824f27a88773385b8
Syed Faiz Ahmed <sfahmed129@gmail.com>
2022-05-21 19:48:24 +0500

    PP on 21/05

--- Testing.r
+++ Testing.r
@@ -13,3 +13,25 @@ print("name")
 
 #This is again from Git and Github
 print("Assalam O Alaikum, bro from Git and Github")
+
+x <- 0:6                  # Coersion
+print(x)
+as.logical(x)
+as.numeric(x)
+
+m <- matrix(4:9 , nrow=2 , ncol=3)    # Creating vectors.
+m
+
+dim(m)
+
+attributes(x)
+attributes(m)
+
+m <- matrix(4:9 , nrow=2 , ncol=3)
+m
+
+m <- 1:6                    # Matrices can also be created directly from vectors by adding adding a dimension attribute.
+m
+
+m <- 1:4
+m
