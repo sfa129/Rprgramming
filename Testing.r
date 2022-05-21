c0e63bd6a46db57f34f96f5c48b481f8e434548e
Syed Faiz Ahmed <sfahmed129@gmail.com>
2022-05-21 23:08:31 +0500

    Second Phase on 21/5

--- Testing.r
+++ Testing.r
@@ -35,3 +35,94 @@ m
 
 m <- 1:4
 m
+
+
+x <- 0:6
+class(x)
+
+x <- 5L
+class(x)
+
+x <- 6L
+class(x)
+print(x)
+
+
+# Creating matrices using columnbinding and rowbinding. 
+#row binding (Console Based)
+
+c <- 0:2
+> d <- 3:5
+> 
+  > cbind(c, d)
+c d
+[1,] 0 3
+[2,] 1 4
+[3,] 2 5
+
+
+#row bind (console based)
+f <- 56:59
+> g <- 60:63
+> 
+  > rbind(f, g)
+[,1] [,2] [,3] [,4]
+f   56   57   58   59
+g   60   61   62   63
+
+#list
+#Note: During execution, remember that 1st execute the vector and then list for desired result.
+#use to keep the values in their own state.
+
+#let's creating vector
+f <- c(1, TRUE, "SO", 7)
+f
+
+# output is [1] "1"    "TRUE" "SO"   "7"   
+
+# but with list, we get,
+
+
+f <- list(1, TRUE, "SO", 7)
+f
+
+#output is 
+> f
+[[1]]
+[1] 1
+
+[[2]]
+[1] TRUE
+
+[[3]]
+[1] "SO"
+
+[[4]]
+[1] 7
+
+
+
+
+# Factors: -
+e <- factor(c("Monday" , "Tuesday" , "Wednesday" , "Tuesday" , "Monday"))
+e
+
+#output is [1] Monday    Tuesday   Wednesday Tuesday   Monday   
+Levels: Monday Tuesday Wednesday
+
+table(e)
+#output is Monday   Tuesday Wednesday 
+2         2         1 
+
+unclass(e)
+#output is 
+[1] 1 2 3 2 1
+attr(,"levels")
+[1] "Monday"    "Tuesday"   "Wednesday"
+> 
+ 
+# Factors with 'levels' attribute: -
+  e <- factor(c("Monday" , "Tuesday" , "Wednesday" , "Tuesday" , "Monday"))
+    levels = c("Sunday" , "Monday" , "Tuesday")
+    
+
